adb2ba263e01daa21a081353bdfe22930272e12b
roscas <Stefana.Rosca@msg.group>
2024-05-14 12:26:16 +0300

    add edit page

--- my-app-name-redux/src/components/bases/GenericErrorHandler.jsx
+++ my-app-name-redux/src/components/bases/GenericErrorHandler.jsx
@@ -3,7 +3,7 @@ export const genericErrorHandler = (messageSetter) => {
     return (error) => {
         if (error.response.status === 404) {
             messageSetter('Not found');
-            return Promise.resolve('Not found');
+            return Promise.resolve(null);
         }
         return Promise.resolve(null);
     };
--- my-app-name-redux/src/components/todos/ToDoItem.jsx
+++ my-app-name-redux/src/components/todos/ToDoItem.jsx
@@ -1,20 +1,27 @@
 import React, {useContext} from "react";
-import {Checkbox, Typography} from "@mui/material";
+import {Button, Checkbox, Typography} from "@mui/material";
 import {useSelector} from "react-redux";
 import {toggleDone} from "../../actions/ToDoActions.jsx";
 import {useDispatch} from "react-redux";
+import {useNavigate} from "react-router-dom";
 
 function ToDoItem({index}){
     console.log("rendered todoItem");
     const toDo = useSelector((state)=> state.toDos[index]);
     const dispatch = useDispatch();
+    const navigate= useNavigate();
     const onChange = (event) => {
         dispatch(toggleDone(index));
+    }
+    const onEdit = () => {
+        console.log(toDo.id);
+        navigate(`/edit/${toDo.id}`);
     }
     return (
         <div>
             <Checkbox checked={toDo.done} onChange={onChange}/>
             <Typography variant="h4" key={toDo.title}>{toDo.title}</Typography>
+            <Button onClick={onEdit} >Edit</Button>
         </div>
     );
 }
