0dc0f800d8819b156f1b4d7b802695df45288d7f
roscas <Stefana.Rosca@msg.group>
2024-05-21 11:33:08 +0300

    use form

--- my-app-name-redux/src/routes/EditToDo.jsx
+++ my-app-name-redux/src/routes/EditToDo.jsx
@@ -1,12 +1,35 @@
-import {useLoaderData} from "react-router-dom";
+import {useLoaderData, useNavigate} from "react-router-dom";
+import {useForm} from "react-hook-form";
+import {Button} from "@mui/material";
+import {ToDoService} from "../components/services/ToDoService.jsx";
+import {useInjection} from "inversify-react";
+import {FormFancyTextField} from "../components/bases/formWrappers/FormFancyTextField.jsx";
 
 export function EditToDo() {
-    let { id, title } = useLoaderData();
+    let toDo = useLoaderData();
+    const toDoService = useInjection(ToDoService);
+    const navigate= useNavigate();
+    const {
+        register,
+        handleSubmit,
+        watch,
+        formState: { errors, isDirty},
+        control,
+    } = useForm({defaultValues: toDo, mode: 'all'})
+    const onSubmit = async (data) => {
+        console.log('data: ', data);
+        await toDoService.updateToDoBE(data);
+        navigate('/');
+    }
+    const title = watch('title');
     return (
-        <div>
+        <form onSubmit={handleSubmit(onSubmit)}>
             <h1>Edit Page</h1>
-            <h2>{'id: '+id}</h2>
-            <h2>{'title: '+title}</h2>
-        </div>
+            <h1>{toDo.title}</h1>
+            <h1>{title}</h1>
+            <FormFancyTextField control={control} name={'title'} label={'To Do Title'} limit={200}/>
+            {errors.title && <span>This field is required</span>}
+            <Button disabled={!isDirty} type={'submit'} >Save</Button>
+        </form>
     );
 }
